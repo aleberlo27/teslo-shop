5da8ca23462742382815a616d8c4366f1c8faa0e
Practicas3 <practicas3@servinet.net>
2025-03-31 16:37:19 +0200

    Tarjetas de producto e intento de animación

--- src/app/products/store-front/pages/home-page/home-page.component.ts
+++ src/app/products/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ProductPageComponent } from "../product-page/product-page.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductPageComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent { }
+export class HomePageComponent {
+  products = signal([
+
+  ]);
+
+
+}
--- src/app/products/store-front/pages/product-page/product-page.component.ts
+++ src/app/products/store-front/pages/product-page/product-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './product-page.component.html',
 })
 export class ProductPageComponent { }

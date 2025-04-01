0d4ed75e86915fc52defd41539a2d9e0caf302b1
Practicas3 <practicas3@servinet.net>
2025-04-01 14:54:25 +0200

    Pipe para imágenes, si no tienen imágen el producto sacar una por defecto

--- src/app/products/components/product-card/product-card.component.ts
+++ src/app/products/components/product-card/product-card.component.ts
@@ -2,11 +2,12 @@ import { SlicePipe } from '@angular/common';
 import { Component, computed, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Product } from '@products/interfaces/product.interface';
+import { ProductImagePipe } from '@products/pipes/product-image.pipe';
 import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-product-card',
-  imports: [RouterLink, SlicePipe],
+  imports: [RouterLink, SlicePipe, ProductImagePipe],
   templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
--- src/app/products/store-front/pages/home-page/home-page.component.ts
+++ src/app/products/store-front/pages/home-page/home-page.component.ts
@@ -17,7 +17,6 @@ export class HomePageComponent {
     request: () => ({}),
     loader: ({request}) => {
       return this.productService.getProducts({
-        gender: 'men',
       });
     },
   });

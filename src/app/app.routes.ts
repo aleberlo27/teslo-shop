289c273406c83bcbbc0c7a632064717d04881845
Practicas3 <practicas3@servinet.net>
2025-04-09 14:59:37 +0200

    Creacion de carpetas y diseño panel admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,10 @@ export const routes: Routes = [
     ]
   },
   {
+    path:'admin',
+    loadChildren: () => import('./admin-dashboard/admin-dashboard.routes'),
+  },
+  {
     path: '',
     loadChildren: () => import('./products/store-front/store-front.routes'),
   },

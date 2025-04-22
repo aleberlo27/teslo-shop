e1475d9de18275ca68bdf4576f7564a4fcb04f80
Practicas3 <practicas3@servinet.net>
2025-04-22 13:23:48 +0200

    Actualizar caché

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -67,7 +67,26 @@ export class ProductsService {
     id: string,
     productLike: Partial<Product>
   ): Observable<Product>{
-    return this.http.patch<Product>(`${baseUrl}/products/${id}`, productLike);
+    return this.http.patch<Product>(`${baseUrl}/products/${id}`, productLike)
+    .pipe(
+      tap((product) => this.updateProductCache(product)),
+    );
+  }
+
+  updateProductCache(product: Product){
+    const productId = product.id;
+
+    this.productCache.set(productId, product);
+
+    this.productsCache.forEach(
+      (productResponse) => {
+      productResponse.products = productResponse.products.map(
+        (currentProduct) =>{
+        return currentProduct.id == productId ? product : currentProduct
+      });
+    });
+
+    console.log('Caché actualizado.')
   }
 
 }
